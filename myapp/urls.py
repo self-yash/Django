820f62575cf995e56f41c2523a8449e9652f70da
self-yash <yashmehta0005@gmail.com>
2025-09-27 21:46:49 +0530

    added authentication via JWT

--- myapp/urls.py
+++ myapp/urls.py
@@ -3,10 +3,13 @@ from django.urls import path, include
 from rest_framework import routers
 from . import views
 
+#all urls here will be http://127.0.0.1:8000/api/
+
 urlpatterns = [
     path('students/create/',views.create_student,name='create_student'),
     path('students/list/',views.list_student,name='list_student'),
     path('students/view/<int:rollno>/',views.get_student,name='get_student'),
     path('students/delete/<int:rollno>/',views.delete_student,name='delete_student'),
     path('students/update/<int:rollno>/',views.update_student,name='update_student'),
+    path('protected/',views.protected_view,name='protected_view')
 ]
