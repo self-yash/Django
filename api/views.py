c61bc61b443a41683de02f83e537bac9e7d9c202
self-yash <yashmehta0005@gmail.com>
2025-10-14 13:05:46 +0530

    used rest framework decorators

--- api/views.py
+++ api/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
 from .serializers import ProductSerializer
 from .models import Product
-from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 # Create your views here.
 
+@api_view(['GET'])
 def product_list(request):
     products = Product.objects.all()
     serializer= ProductSerializer(products,many=True)
-    return JsonResponse({
-        'data': serializer.data
-    })
+    return Response(serializer.data)
+
+def product_detail(request,pk):
