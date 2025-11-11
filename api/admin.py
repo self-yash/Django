fc886eeb71d0a8d714cea6bdac923928a88527e8
self-yash <yashmehta0005@gmail.com>
2025-11-11 19:00:25 +0530

    added admin panel created su and added a new order using ad

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from api.models import Order, OrderItem
 
 # Register your models here.
+
+class OrderItemAdmin(admin.TabularInline):
+    model = OrderItem
+
+class OrderAdmin(admin.ModelAdmin):
+    inlines = [
+        OrderItemAdmin
+    ]
+
+admin.site.register(Order,OrderAdmin)
